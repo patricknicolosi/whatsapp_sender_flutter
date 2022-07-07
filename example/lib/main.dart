a5bd27ae9e6c0410ea1d18ace5df635230383b1e
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-07-07 18:22:48 +0200

    Prepare for publish

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
-import 'package:whatsapp_sender/whatsapp_sender.dart';
+import 'package:whatsapp_sender_flutter/whatsapp_sender_flutter.dart';
 
 void main() {
   runApp(
