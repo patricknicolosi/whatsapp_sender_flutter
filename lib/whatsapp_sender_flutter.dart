1b9e3e55f94bc17ba90e898adbced6f76fd6c5b5
Patrick Nicolosi <patricknicolosi99@gmail.com>
2022-07-10 20:19:43 +0200

    Bug fix

--- lib/whatsapp_sender_flutter.dart
+++ lib/whatsapp_sender_flutter.dart
@@ -22,14 +22,74 @@ class WhatsAppSenderFlutter {
       required String message,
       String? savedSessionDir}) async {
     _initializingStatusVariables();
-    await _openWhatsAppWeb(savedSessionDir);
-    await _readQrcode();
-    await _waitChatScreen(savedSessionDir);
+    try {
+      _browser = await puppeteer.launch(
+        headless: true,
+        noSandboxFlag: true,
+        args: ['--disable-setuid-sandbox'],
+        userDataDir: savedSessionDir,
+      );
+      _page = await _browser.newPage();
+      const userAgent =
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+      await _page.setUserAgent(userAgent);
+      await _page.goto('http://web.whatsapp.com');
+    } catch (e) {
+      status.value = WhatsAppSenderFlutterStatusMessage.errorOnLaunch;
+      await close();
+      return;
+    }
+    try {
+      await Future.delayed(const Duration(seconds: 3));
+      await _page
+          .evaluate(
+              '() => document.querySelector("div[data-ref]").getAttribute("data-ref")')
+          .then((qrCodeData) {
+        qrCode.value = qrCodeData;
+        status.value = WhatsAppSenderFlutterStatusMessage.scanQrCode;
+      });
+    } catch (e) {
+      log(e.toString());
+    }
+    try {
+      await _page.waitForSelector(
+        '.zaKsw',
+        timeout: const Duration(milliseconds: 60000),
+      );
+    } catch (e) {
+      if (savedSessionDir == null || savedSessionDir.isEmpty) {
+        status.value = WhatsAppSenderFlutterStatusMessage.qrCodeExpirated;
+        await close();
+        return;
+      }
+    }
+    qrCode.value = "";
     for (int i = 0; i < phones.length; i++) {
-      await _sendMessage(phones[i], message);
+      try {
+        status.value = WhatsAppSenderFlutterStatusMessage.sending;
+        await _page.goto(
+            'https://web.whatsapp.com/send?phone=${phones[i]}&text=$message');
+        var onDialog = _page.onDialog.listen((event) {
+          event.accept();
+        }, cancelOnError: true);
+        onDialog.onDone(() {
+          onDialog.cancel();
+        });
+        await _page.waitForSelector("progress",
+            hidden: true, timeout: const Duration(milliseconds: 60000));
+        await _page.waitForSelector(
+            'div:nth-child(2) > button > span[data-icon="send"]',
+            timeout: const Duration(milliseconds: 60000));
+        await _page.keyboard.press(Key.enter);
+        await Future.delayed(const Duration(milliseconds: 1000));
+        success.value = success.value + 1;
+      } catch (e) {
+        fails.value = fails.value + 1;
+      }
     }
     status.value = WhatsAppSenderFlutterStatusMessage.done;
     await close();
+    return;
   }
 
   Future close() async {
@@ -46,84 +106,11 @@ class WhatsAppSenderFlutter {
     qrCode.value = "";
     status.value = WhatsAppSenderFlutterStatusMessage.initializing;
   }
-
-  Future<void> _sendMessage(String phone, String message) async {
-    try {
-      status.value = WhatsAppSenderFlutterStatusMessage.sending;
-      await _page
-          .goto('https://web.whatsapp.com/send?phone=$phone&text=$message');
-      var onDialog = _page.onDialog.listen((event) {
-        event.accept();
-      }, cancelOnError: true);
-      onDialog.onDone(() {
-        onDialog.cancel();
-      });
-      await _page.waitForSelector("progress",
-          hidden: true, timeout: const Duration(milliseconds: 60000));
-      await _page.waitForSelector(
-          'div:nth-child(2) > button > span[data-icon="send"]',
-          timeout: const Duration(milliseconds: 60000));
-      await _page.keyboard.press(Key.enter);
-      await Future.delayed(const Duration(milliseconds: 1000));
-      success.value = success.value + 1;
-    } catch (e) {
-      fails.value = fails.value + 1;
-    }
-  }
-
-  Future<void> _waitChatScreen(String? savedSessionDir) async {
-    try {
-      qrCode.value = "";
-      await _page.waitForSelector(
-        '.zaKsw',
-        timeout: const Duration(milliseconds: 60000),
-      );
-    } catch (e) {
-      if (savedSessionDir == null || savedSessionDir.isEmpty) {
-        status.value = WhatsAppSenderFlutterStatusMessage.qrCodeExpirated;
-        await close();
-      } else {
-        await close();
-      }
-      return;
-    }
-  }
-
-  Future<void> _readQrcode() async {
-    await Future.delayed(const Duration(seconds: 3));
-    try {
-      await _page
-          .evaluate(
-              '() => document.querySelector("div[data-ref]").getAttribute("data-ref")')
-          .then((qrCodeData) {
-        qrCode.value = qrCodeData;
-      });
-    } catch (e) {
-      log(e.toString());
-    }
-  }
-
-  Future<void> _openWhatsAppWeb(String? savedSessionDir) async {
-    try {
-      _browser = await puppeteer.launch(
-        headless: true,
-        noSandboxFlag: true,
-        args: ['--disable-setuid-sandbox'],
-        userDataDir: savedSessionDir,
-      );
-      _page = await _browser.newPage();
-      const userAgent =
-          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
-      await _page.setUserAgent(userAgent);
-      await _page.goto('http://web.whatsapp.com');
-    } catch (e) {
-      log(e.toString());
-    }
-  }
 }
 
 class WhatsAppSenderFlutterStatusMessage {
   static const String initializing = "initializing";
+  static const String errorOnLaunch = "error on launch";
   static const String scanQrCode = "scan qr code";
   static const String sending = "sending";
   static const String done = "done";
